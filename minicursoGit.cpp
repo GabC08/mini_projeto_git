b40aa7e888d463b78540b80ba6bb1ee0827a9ec7
GabC08 <32557253+GabC08@users.noreply.github.com>
2017-10-05 20:40:23 -0300

    Update minicursoGit.cpp
    asd

--- minicursoGit.cpp
+++ minicursoGit.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+//editado
 int main(int argc, char** argv) {
 	return 0;
 }
